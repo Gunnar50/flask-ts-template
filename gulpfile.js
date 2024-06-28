7bad5ecdabd4dec2ca7864053072be879ed4836c
gustavo <gustavo.passarella@hotmail.com>
2024-06-28 15:47:50 +0100

    Update to use min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
+    .pipe(rename('index.min.css'))
     .pipe(gulp.dest('src/static/compiled'));
 });
 
